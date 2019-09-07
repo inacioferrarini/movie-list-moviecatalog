960b0b03933adf99cfed331e7b817c91e0c73665
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2019-09-07 13:12:36 -0300

    Initial version

---
+++ MovieCatalog/MovieCatalog.h
@@ -0,0 +1,19 @@
+//
+//  MovieCatalog.h
+//  MovieCatalog
+//
+//  Created by José Inácio Athayde Ferrarini on 07/09/19.
+//  Copyright © 2019 José Inácio Athayde Ferrarini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MovieCatalog.
+FOUNDATION_EXPORT double MovieCatalogVersionNumber;
+
+//! Project version string for MovieCatalog.
+FOUNDATION_EXPORT const unsigned char MovieCatalogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MovieCatalog/PublicHeader.h>
+
+
