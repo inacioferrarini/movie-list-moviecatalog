885aa736ac575dd1090a1144f90c3f33967a544c
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2019-09-17 01:54:51 -0300

    Update cells

--- MovieCatalog/Cells/MovieCollectionViewCell.swift
+++ MovieCatalog/Cells/MovieCollectionViewCell.swift
@@ -3,12 +3,26 @@ import Common
 
 class MovieCollectionViewCell: UICollectionViewCell, Configurable {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-    }
-
+    // MARK: - Outlets
+    
+    @IBOutlet weak private(set) var posterImage: UIImageView!
+    @IBOutlet weak private(set) var titleLabel: UILabel!
+    @IBOutlet weak private(set) var favoriteStatusImage: UIImageView!
+    
+    // MARK: - Setup
+    
     func setup(with value: Movie) {
-        print("MovieCollectionViewCell setup with \(value)")
+        if let posterImageUrl = URL(string: "http://image.tmdb.org/t/p/w185//" + value.posterPath) {
+            DispatchQueue.global().async {
+                if let posterImageData = try? Data(contentsOf: posterImageUrl) {
+                    DispatchQueue.main.async {
+                        self.posterImage.image = UIImage(data: posterImageData)
+                    }
+                }
+            }
+        }
+        titleLabel.text = value.title
+        favoriteStatusImage.image = Assets.Icons.Status.favoriteGray
     }
     
 }
