378739f69d38ecb486dc15fa47f56fa33d622311
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2019-09-21 01:16:02 -0300

    Adds new properties. They became optionals.

--- MovieCatalog/Models/Movie.swift
+++ MovieCatalog/Models/Movie.swift
@@ -2,8 +2,10 @@ import Foundation
 
 struct Movie: Codable, Equatable {
     
-    var title: String
-    var overview: String
-    var posterPath: String
+    var title: String?
+    var overview: String?
+    var posterPath: String?
+    var genreIds: [Int]?
+    var releaseDate: String?
     
 }
